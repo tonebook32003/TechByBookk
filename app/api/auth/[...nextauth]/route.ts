1a490e75cc6c6660872214a509e47c079ed051b3
Tôn Sách ( Bookk ) <tonsach32003@gmail.com>
2025-12-01 21:09:28 +0700

    config NextAuth

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,13 @@
 import NextAuth from "next-auth";
-import { authOptions } from "@/lib/auth";
+import GoogleProvider from "next-auth/providers/google";
 
-const handler = NextAuth(authOptions);
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+});
 
 export { handler as GET, handler as POST };
